fe673f2036ed984e9fcb8238633d4ae8ff69eb70
sboyanovich <sergej.bojanovic3214@gmail.com>
2019-04-05 23:31:44 +0300

    Made NFAStateGraph immutable and wrote a builder for it.

--- src/io/github/sboyanovich/scannergenerator/automata/NFAStateGraphBuilder.java
+++ src/io/github/sboyanovich/scannergenerator/automata/NFAStateGraphBuilder.java
@@ -2,29 +2,41 @@ package io.github.sboyanovich.scannergenerator.automata;
 
 import java.util.*;
 
-/**
- * Represent state graph for automaton with 'numberOfStates' states.
- */
-public class NFAStateGraph {
+public class NFAStateGraphBuilder {
     private int numberOfStates;
     private List<List<Optional<Set<Integer>>>> edges;
 
-    // created only through builder
-    NFAStateGraph(int numberOfStates, List<List<Optional<Set<Integer>>>> edges) {
+    public NFAStateGraphBuilder(int numberOfStates) {
         this.numberOfStates = numberOfStates;
-        this.edges = new ArrayList<>();
+        this.edges = new ArrayList<>(numberOfStates);
+        /* Initializing edge matrix. */
+        /*
+         * no set means no edge
+         * empty set means lambda-step
+         * nonempty set represents letters that enable transition
+         */
         for (int i = 0; i < numberOfStates; i++) {
-            this.edges.add(new ArrayList<>());
+            this.edges.add(new ArrayList<>(numberOfStates));
             for (int j = 0; j < numberOfStates; j++) {
-                this.edges.get(i).add(
-                        edges.get(i).get(j).map(HashSet::new)
-                ); // defensive copy
+                this.edges.get(i).add(Optional.empty());
             }
         }
     }
 
     public int getNumberOfStates() {
         return numberOfStates;
+    }
+
+    public void setEdge(int i, int j, Set<Integer> marker) {
+        // validate
+        // null marker not allowed
+        // defensive copy
+        this.edges.get(i).set(j, Optional.of(new HashSet<>(marker)));
+    }
+
+    public void removeEdge(int i, int j) {
+        //validate
+        this.edges.get(i).set(j, Optional.empty());
     }
 
     public boolean edgeExists(int i, int j) {
@@ -52,5 +64,9 @@ public class NFAStateGraph {
     private Optional<Set<Integer>> getEdgeMarkerAux(int i, int j) {
         // validate
         return this.edges.get(i).get(j);
+    }
+
+    public NFAStateGraph build() {
+        return new NFAStateGraph(this.numberOfStates, this.edges);
     }
 }
