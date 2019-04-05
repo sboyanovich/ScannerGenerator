fef08b087a24b8ddb844ccbf36a7e50d7bcacf38
sboyanovich <sergej.bojanovic3214@gmail.com>
2019-04-05 23:50:19 +0300

    Added standard StateTag for non-final states.

---
+++ src/io/github/sboyanovich/scannergenerator/lex/STNotFinal.java
@@ -0,0 +1,17 @@
+package io.github.sboyanovich.scannergenerator.lex;
+
+import io.github.sboyanovich.scannergenerator.token.Domain;
+
+public enum STNotFinal implements StateTag {
+    NOT_FINAL {
+        @Override
+        public boolean isFinal() {
+            return false;
+        }
+
+        @Override
+        public Domain getDomain() {
+            throw new Error("This is never supposed to be called!");
+        }
+    }
+}
