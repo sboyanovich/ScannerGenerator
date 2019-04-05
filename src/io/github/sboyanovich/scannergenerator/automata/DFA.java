e27f956a9d2622449c8a25851bcce89c73aced92
sboyanovich <sergej.bojanovic3214@gmail.com>
2019-04-05 22:37:45 +0300

    Replaced InvalidParameterException throws with IllegalArgumentExceptions in DFA.

--- src/io/github/sboyanovich/scannergenerator/automata/DFA.java
+++ src/io/github/sboyanovich/scannergenerator/automata/DFA.java
@@ -2,7 +2,6 @@ package io.github.sboyanovich.scannergenerator.automata;
 
 import io.github.sboyanovich.scannergenerator.lex.StateTag;
 
-import java.security.InvalidParameterException;
 import java.util.*;
 
 import static io.github.sboyanovich.scannergenerator.utility.Utility.copyTable;
@@ -42,37 +41,37 @@ public class DFA {
         //  transitionTable elements in [0, numberOfStates - 1]
 
         if (!(numberOfStates > 0)) {
-            throw new InvalidParameterException("Number of states must be non-negative!");
+            throw new IllegalArgumentException("Number of states must be non-negative!");
         }
         if (!(alphabetSize > 0)) {
-            throw new InvalidParameterException("Alphabet size must be non-negative!");
+            throw new IllegalArgumentException("Alphabet size must be non-negative!");
         }
         if (!isInRange(initialState, 0, numberOfStates - 1)) {
-            throw new InvalidParameterException("Initial state must be in range [0, numberOfStates-1]!");
+            throw new IllegalArgumentException("Initial state must be in range [0, numberOfStates-1]!");
         }
         for (int state : acceptingStates) {
             if (!isInRange(state, 0, numberOfStates - 1)) {
-                throw new InvalidParameterException("Accepting states must be in range [0, numberOfStates-1]!");
+                throw new IllegalArgumentException("Accepting states must be in range [0, numberOfStates-1]!");
             }
         }
         for (int i = 0; i < numberOfStates; i++) {
             if (!labelsMap.containsKey(i)) {
-                throw new InvalidParameterException("All states should be labeled!");
+                throw new IllegalArgumentException("All states should be labeled!");
             }
         }
         if (transitionTable.length != numberOfStates) {
-            throw new InvalidParameterException("Transition table should have 'numberOfStates' rows!");
+            throw new IllegalArgumentException("Transition table should have 'numberOfStates' rows!");
         }
         for (int i = 0; i < numberOfStates; i++) {
             if (transitionTable[i].length != alphabetSize) {
-                throw new InvalidParameterException("Transition table should have 'alphabetSize' columns!");
+                throw new IllegalArgumentException("Transition table should have 'alphabetSize' columns!");
             }
         }
         for (int i = 0; i < numberOfStates; i++) {
             for (int j = 0; j < alphabetSize; j++) {
                 int state = transitionTable[i][j];
                 if (!isInRange(state, 0, numberOfStates - 1)) {
-                    throw new InvalidParameterException(
+                    throw new IllegalArgumentException(
                             "Transition table should point to states in range [0, numberOfStates-1]!");
                 }
             }
