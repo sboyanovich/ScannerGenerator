ce7deeaea176459bf0545a5c870aa0895aadf4ef
sboyanovich <sergej.bojanovic3214@gmail.com>
2019-04-06 22:41:00 +0300

    Added standard static isFinal method to StateTag.

--- src/io/github/sboyanovich/scannergenerator/lex/StateTag.java
+++ src/io/github/sboyanovich/scannergenerator/lex/StateTag.java
@@ -3,5 +3,8 @@ package io.github.sboyanovich.scannergenerator.lex;
 import io.github.sboyanovich.scannergenerator.token.Domain;
 
 public interface StateTag {
+    static boolean isFinal(StateTag tag) {
+        return !tag.equals(STNotFinal.NOT_FINAL);
+    }
     Domain getDomain();
 }
