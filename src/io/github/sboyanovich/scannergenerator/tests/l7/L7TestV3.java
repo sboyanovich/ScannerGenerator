26bfc56576f772502a5b90380ac28eb5b61e2b0b
sboyanovich <sergej.bojanovic3214@gmail.com>
2019-04-14 20:11:26 +0300

    Testing generator on L7 for my Compiler Construction course.

---
+++ src/io/github/sboyanovich/scannergenerator/tests/l7/L7TestV3.java
@@ -0,0 +1,441 @@
+package io.github.sboyanovich.scannergenerator.tests.l7;
+
+import io.github.sboyanovich.scannergenerator.Position;
+import io.github.sboyanovich.scannergenerator.automata.NFA;
+import io.github.sboyanovich.scannergenerator.lex.Compiler;
+import io.github.sboyanovich.scannergenerator.lex.*;
+import io.github.sboyanovich.scannergenerator.lex.Scanner;
+import io.github.sboyanovich.scannergenerator.tests.data.domains.DomainsWithStringAttribute;
+import io.github.sboyanovich.scannergenerator.tests.data.domains.SimpleDomains;
+import io.github.sboyanovich.scannergenerator.token.Domain;
+import io.github.sboyanovich.scannergenerator.token.DomainEOP;
+import io.github.sboyanovich.scannergenerator.token.DomainError;
+import io.github.sboyanovich.scannergenerator.token.Token;
+import io.github.sboyanovich.scannergenerator.utility.Pair;
+import io.github.sboyanovich.scannergenerator.utility.Utility;
+
+import java.util.*;
+
+import static io.github.sboyanovich.scannergenerator.tests.data.CommonCharClasses.alphanumerics;
+import static io.github.sboyanovich.scannergenerator.tests.data.CommonCharClasses.letters;
+import static io.github.sboyanovich.scannergenerator.tests.data.states.StateTags.*;
+import static io.github.sboyanovich.scannergenerator.utility.Utility.*;
+
+public class L7TestV3 {
+    public static void main(String[] args) {
+        int alphabetSize = Character.MAX_CODE_POINT + 1;
+        /*
+            basically, n^2 complexity of brute force emap building is prohibitive
+            when dealing with entire Unicode span
+        */
+        //alphabetSize = 2 * Short.MAX_VALUE + 1;
+        alphabetSize = 255; // special case hack for faster recognizer generation
+
+        NFA spaceNFA = NFA.singleLetterLanguage(alphabetSize, asCodePoint(" "));
+        NFA tabNFA = NFA.singleLetterLanguage(alphabetSize, asCodePoint("\t"));
+        NFA newlineNFA = NFA.singleLetterLanguage(alphabetSize, asCodePoint("\n"));
+        NFA carretNFA = NFA.singleLetterLanguage(alphabetSize, asCodePoint("\r"));
+
+        NFA slWhitespaceNFA = spaceNFA
+                .union(tabNFA)
+                .positiveIteration();
+
+        NFA whitespaceNFA = spaceNFA
+                .union(tabNFA)
+                .union(carretNFA.concatenation(newlineNFA))
+                .union(newlineNFA)
+                .positiveIteration()
+                .setAllFinalStatesTo(WHITESPACE);
+
+        NFA lettersNFA = acceptsAllTheseSymbols(alphabetSize, letters);
+        NFA alphanumericsNFA = acceptsAllTheseSymbols(alphabetSize, alphanumerics);
+
+        NFA identifierNFA = lettersNFA
+                .concatenation(alphanumericsNFA.iteration())
+                .setAllFinalStatesTo(IDENTIFIER);
+
+        NFA kwAxiomNFA = acceptThisWord(alphabetSize, List.of("a", "x", "i", "o", "m"));
+        NFA lparenNFA = NFA.singleLetterLanguage(alphabetSize, asCodePoint("("));
+        NFA rparenNFA = NFA.singleLetterLanguage(alphabetSize, asCodePoint(")"));
+
+        NFA axmDeclNFA = lparenNFA
+                .concatenation(kwAxiomNFA)
+                .concatenation(slWhitespaceNFA)
+                .concatenation(identifierNFA)
+                .concatenation(rparenNFA)
+                .setAllFinalStatesTo(AXM_DECL);
+
+        NFA nonTerminalNFA = lparenNFA
+                .concatenation(identifierNFA)
+                .concatenation(rparenNFA)
+                .setAllFinalStatesTo(NON_TERMINAL);
+
+        NFA opPlusNFA = NFA.singleLetterLanguage(alphabetSize, asCodePoint("+"))
+                .setAllFinalStatesTo(OP_PLUS);
+        NFA opMultiplyNFA = NFA.singleLetterLanguage(alphabetSize, asCodePoint("*"))
+                .setAllFinalStatesTo(OP_MULTIPLY);
+
+        NFA equalsNFA = NFA.singleLetterLanguage(alphabetSize, asCodePoint("="))
+                .setAllFinalStatesTo(EQUALS);
+
+        NFA vbarNFA = NFA.singleLetterLanguage(alphabetSize, asCodePoint("|"))
+                .setAllFinalStatesTo(VERTICAL_BAR);
+
+        NFA dotNFA = NFA.singleLetterLanguage(alphabetSize, asCodePoint("."))
+                .setAllFinalStatesTo(DOT);
+
+        NFA eslpNFA = acceptThisWord(alphabetSize, List.of("\\", "("))
+                .setAllFinalStatesTo(ESCAPED_LPAREN);
+
+        NFA esrpNFA = acceptThisWord(alphabetSize, List.of("\\", ")"))
+                .setAllFinalStatesTo(ESCAPED_RPAREN);
+
+        NFA lang = whitespaceNFA
+                .union(identifierNFA)
+                .union(axmDeclNFA)
+                .union(nonTerminalNFA)
+                .union(opPlusNFA)
+                .union(opMultiplyNFA)
+                .union(equalsNFA)
+                .union(vbarNFA)
+                .union(dotNFA)
+                .union(eslpNFA)
+                .union(esrpNFA);
+
+        List<StateTag> priorityList = List.of(
+                WHITESPACE,
+                IDENTIFIER,
+                AXM_DECL,
+                NON_TERMINAL,
+                OP_MULTIPLY,
+                OP_PLUS,
+                DOT,
+                VERTICAL_BAR,
+                EQUALS,
+                ESCAPED_LPAREN,
+                ESCAPED_RPAREN
+        );
+
+        Map<StateTag, Integer> priorityMap = new HashMap<>();
+        for (int i = 0; i < priorityList.size(); i++) {
+            priorityMap.put(priorityList.get(i), i);
+        }
+
+        System.out.println(lang.getNumberOfStates());
+
+        LexicalRecognizer recognizer = Utility.createRecognizer(lang, priorityMap);
+        System.out.println("Recognizer built!");
+
+        String dot = recognizer.toGraphvizDotString(Object::toString, true);
+        System.out.println(dot);
+
+        String text = Utility.getText("l7test.txt");
+
+        Compiler compiler = new Compiler(recognizer);
+        Scanner scanner = compiler.getScanner(text);
+
+        Set<Domain> ignoredTokenTypes = Set.of(
+                SimpleDomains.WHITESPACE,
+                DomainEOP.END_OF_PROGRAM,
+                DomainError.ERROR
+        );
+
+        int errCount = 0;
+
+        List<Token> tokensToParse = new ArrayList<>();
+
+        Token t = scanner.nextToken();
+        while (t.getTag() != DomainEOP.END_OF_PROGRAM) {
+            if (!ignoredTokenTypes.contains(t.getTag())) {
+                tokensToParse.add(t);
+                System.out.println(t);
+            }
+            if (t.getTag() == DomainError.ERROR) {
+                errCount++;
+                System.out.println(t.getCoords());
+            }
+            t = scanner.nextToken();
+        }
+        tokensToParse.add(t);
+
+        System.out.println();
+        System.out.println("Errors: " + errCount);
+        System.out.println("Compiler messages: ");
+        SortedMap<Position, Message> messages = compiler.getSortedMessages();
+        for (Map.Entry<Position, Message> entry : messages.entrySet()) {
+            System.out.println(entry.getValue() + " at " + entry.getKey());
+        }
+
+        List<Domain> columns = List.of(
+                DomainsWithStringAttribute.AXM_DECL,
+                DomainsWithStringAttribute.IDENTIFIER,
+                DomainsWithStringAttribute.NON_TERMINAL,
+                SimpleDomains.EQUALS,
+                SimpleDomains.VERTICAL_BAR,
+                SimpleDomains.DOT,
+                SimpleDomains.OP_PLUS,
+                SimpleDomains.OP_MULTIPLY,
+                SimpleDomains.ESCAPED_LPAREN,
+                SimpleDomains.ESCAPED_RPAREN,
+                DomainEOP.END_OF_PROGRAM
+        );
+
+        List<String> nonTerminalNames = List.of(
+                "<lang>",
+                "<rule_list>",
+                "<rule>",
+                "<rhs_list>",
+                "<rhs_list_c>",
+                "<rhs>",
+                "<tl>",
+                "<t>",
+                "<lhs>"
+        );
+
+        Map<Integer, Map<Domain, Integer>> table = new HashMap<>();
+        Map<Integer, Map<Integer, List<Pair<Integer, Domain>>>> rules = new HashMap<>();
+
+        table.put(0, Map.of(
+                DomainsWithStringAttribute.AXM_DECL, 1,
+                DomainsWithStringAttribute.NON_TERMINAL, 1
+        ));
+
+        rules.put(0, Map.of(
+                1, List.of(
+                        new Pair<>(2, null),
+                        new Pair<>(1, null)
+                )
+        ));
+
+        table.put(1, Map.of(
+                DomainsWithStringAttribute.AXM_DECL, 1,
+                DomainsWithStringAttribute.NON_TERMINAL, 1,
+                DomainEOP.END_OF_PROGRAM, 2
+        ));
+
+        rules.put(1, Map.of(
+                1, List.of(
+                        new Pair<>(2, null),
+                        new Pair<>(1, null)
+                ),
+                2, List.of()
+        ));
+
+        table.put(2, Map.of(
+                DomainsWithStringAttribute.AXM_DECL, 1,
+                DomainsWithStringAttribute.NON_TERMINAL, 1
+        ));
+
+        rules.put(2, Map.of(
+                1, List.of(
+                        new Pair<>(8, null),
+                        new Pair<>(null, SimpleDomains.EQUALS),
+                        new Pair<>(3, null),
+                        new Pair<>(null, SimpleDomains.DOT)
+                )
+        ));
+
+        table.put(3, Map.of(
+                DomainsWithStringAttribute.IDENTIFIER, 1,
+                DomainsWithStringAttribute.NON_TERMINAL, 1,
+                SimpleDomains.VERTICAL_BAR, 1,
+                SimpleDomains.DOT, 1,
+                SimpleDomains.OP_PLUS, 1,
+                SimpleDomains.OP_MULTIPLY, 1,
+                SimpleDomains.ESCAPED_LPAREN, 1,
+                SimpleDomains.ESCAPED_RPAREN, 1
+        ));
+
+        rules.put(3, Map.of(
+                1, List.of(
+                        new Pair<>(5, null),
+                        new Pair<>(4, null)
+                )
+        ));
+
+        table.put(4, Map.of(
+                SimpleDomains.VERTICAL_BAR, 1,
+                SimpleDomains.DOT, 2
+        ));
+
+        rules.put(4, Map.of(
+                1, List.of(
+                        new Pair<>(null, SimpleDomains.VERTICAL_BAR),
+                        new Pair<>(3, null)
+                ),
+                2, List.of()
+        ));
+
+        table.put(5, Map.of(
+                DomainsWithStringAttribute.IDENTIFIER, 1,
+                DomainsWithStringAttribute.NON_TERMINAL, 1,
+                SimpleDomains.VERTICAL_BAR, 2,
+                SimpleDomains.DOT, 2,
+                SimpleDomains.OP_PLUS, 1,
+                SimpleDomains.OP_MULTIPLY, 1,
+                SimpleDomains.ESCAPED_LPAREN, 1,
+                SimpleDomains.ESCAPED_RPAREN, 1
+        ));
+
+        rules.put(5, Map.of(
+                1, List.of(
+                        new Pair<>(7, null),
+                        new Pair<>(6, null)
+                ),
+                2, List.of()
+        ));
+
+        table.put(6, Map.of(
+                DomainsWithStringAttribute.IDENTIFIER, 1,
+                DomainsWithStringAttribute.NON_TERMINAL, 1,
+                SimpleDomains.VERTICAL_BAR, 2,
+                SimpleDomains.DOT, 2,
+                SimpleDomains.OP_PLUS, 1,
+                SimpleDomains.OP_MULTIPLY, 1,
+                SimpleDomains.ESCAPED_LPAREN, 1,
+                SimpleDomains.ESCAPED_RPAREN, 1
+        ));
+
+        rules.put(6, Map.of(
+                1, List.of(
+                        new Pair<>(7, null),
+                        new Pair<>(6, null)
+                ),
+                2, List.of()
+        ));
+
+        table.put(7, Map.of(
+                DomainsWithStringAttribute.IDENTIFIER, 1,
+                DomainsWithStringAttribute.NON_TERMINAL, 2,
+                SimpleDomains.OP_PLUS, 5,
+                SimpleDomains.OP_MULTIPLY, 6,
+                SimpleDomains.ESCAPED_LPAREN, 3,
+                SimpleDomains.ESCAPED_RPAREN, 4
+        ));
+
+        rules.put(7, Map.of(
+                1, List.of(
+                        new Pair<>(null, DomainsWithStringAttribute.IDENTIFIER)
+                ),
+                2, List.of(
+                        new Pair<>(null, DomainsWithStringAttribute.NON_TERMINAL)
+                ),
+                3, List.of(
+                        new Pair<>(null, SimpleDomains.ESCAPED_LPAREN)
+                ),
+                4, List.of(
+                        new Pair<>(null, SimpleDomains.ESCAPED_RPAREN)
+                ),
+                5, List.of(
+                        new Pair<>(null, SimpleDomains.OP_PLUS)
+                ),
+                6, List.of(
+                        new Pair<>(null, SimpleDomains.OP_MULTIPLY)
+                )
+        ));
+
+        table.put(8, Map.of(
+                DomainsWithStringAttribute.AXM_DECL, 2,
+                DomainsWithStringAttribute.NON_TERMINAL, 1
+        ));
+
+        rules.put(8, Map.of(
+                1, List.of(
+                        new Pair<>(null, DomainsWithStringAttribute.NON_TERMINAL)
+                ),
+                2, List.of(
+                        new Pair<>(null, DomainsWithStringAttribute.AXM_DECL)
+                )
+        ));
+
+        ParseTree derivation = parse(tokensToParse, nonTerminalNames, table, rules);
+        dot = derivation.toGraphvizDotString(nonTerminalNames::get);
+        System.out.println(dot);
+    }
+
+    static boolean isTerminal(Pair<Integer, Domain> genSymbol) {
+        return genSymbol.getFirst() == null;
+    }
+
+    static ParseTree parse(
+            List<Token> tokens,
+            List<String> nonTerminalNames,
+            Map<Integer, Map<Domain, Integer>> predictionTable,
+            Map<Integer, Map<Integer, List<Pair<Integer, Domain>>>> rules
+    ) {
+        ParseTree result = new ParseTree(0); // knowing axiom is 0 here
+
+        Deque<Pair<Integer, Domain>> stack = new ArrayDeque<>();
+        Deque<ParseTree.Node> nodeStack = new ArrayDeque<>();
+
+        stack.push(new Pair<>(null, DomainEOP.END_OF_PROGRAM));
+        stack.push(new Pair<>(0, null));
+
+        nodeStack.push(new ParseTree.TerminalNode(null, -1)); // dummy for symmetry
+        nodeStack.push(result.getRoot());
+
+        int cnt = 0; // input tracker
+        int nodeCnt = 1; // node numbering
+
+        while (!stack.isEmpty()) {
+            Pair<Integer, Domain> genSymbol = stack.pop();
+            Token curr = tokens.get(cnt);
+            if (isTerminal(genSymbol)) {
+                ParseTree.TerminalNode leaf = (ParseTree.TerminalNode) nodeStack.pop();
+                Domain expected = genSymbol.getSecond();
+                if (expected != curr.getTag()) {
+                    throw new IllegalArgumentException("Unexpected token encountered: expected type " +
+                            expected + ", got " + curr
+                    );
+                } else {
+                    leaf.setSymbol(curr);
+                    cnt++;
+                }
+            } else {
+                Integer nonTerminal = genSymbol.getFirst();
+                Integer ruleNo = predictionTable.get(nonTerminal).get(curr.getTag());
+                if (ruleNo == null) {
+                    throw new IllegalArgumentException("Token type " + curr.getTag() +
+                            " not allowed here!\n" +
+                            "Violating (token, nonTerminal) pair: " +
+                            "(" + curr + ", " + nonTerminalNames.get(nonTerminal) + ")");
+                } else {
+                    ParseTree.NonTerminalNode subtree = (ParseTree.NonTerminalNode) nodeStack.pop();
+
+                    String ruleApplied = nonTerminalNames.get(nonTerminal) + "_" + ruleNo;
+                    // System.out.println(ruleApplied);
+
+                    List<ParseTree.Node> nodes = new ArrayList<>();
+                    List<Pair<Integer, Domain>> rhs = rules.get(nonTerminal).get(ruleNo);
+                    for (int i = rhs.size() - 1; i >= 0; i--) {
+                        Pair<Integer, Domain> gs = rhs.get(i);
+                        stack.push(gs);
+                        if (!isTerminal(gs)) {
+                            ParseTree.NonTerminalNode nonTerminalNode =
+                                    new ParseTree.NonTerminalNode(gs.getFirst(), nodeCnt);
+                            nodeCnt++;
+                            nodeStack.push(
+                                    nonTerminalNode
+                            );
+                            nodes.add(nonTerminalNode);
+                        } else {
+                            // for now unknown token
+                            ParseTree.TerminalNode terminalNode = new ParseTree.TerminalNode(null, nodeCnt);
+                            nodeCnt++;
+                            nodeStack.push(
+                                    terminalNode
+                            );
+                            nodes.add(terminalNode);
+                        }
+                    }
+
+                    Collections.reverse(nodes);
+
+                    subtree.setChildren(nodes);
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/io/github/sboyanovich/scannergenerator/tests/l7/ParseTree.java
@@ -0,0 +1,120 @@
+package io.github.sboyanovich.scannergenerator.tests.l7;
+
+import io.github.sboyanovich.scannergenerator.token.Token;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+import static io.github.sboyanovich.scannergenerator.utility.Utility.*;
+
+/* Mutable */
+public class ParseTree {
+    interface Node {
+        int getNumber();
+    }
+
+    static class TerminalNode implements Node {
+        private Token symbol;
+        private int number;
+
+        public TerminalNode(Token symbol, int number) {
+            this.symbol = symbol;
+            this.number = number;
+        }
+
+        public Token getSymbol() {
+            return symbol;
+        }
+
+        public void setSymbol(Token symbol) {
+            this.symbol = symbol;
+        }
+
+        @Override
+        public int getNumber() {
+            return this.number;
+        }
+    }
+
+    static class NonTerminalNode implements Node {
+        private int symbol;
+        private int number;
+        private List<Node> children;
+
+        public NonTerminalNode(int symbol, int number) {
+            this.symbol = symbol;
+            this.number = number;
+            this.children = new ArrayList<>();
+        }
+
+        public List<Node> getChildren() {
+            return children;
+        }
+
+        public void setChildren(List<Node> children) {
+            this.children = children;
+        }
+
+        public int getSymbol() {
+            return this.symbol;
+        }
+
+        @Override
+        public int getNumber() {
+            return this.number;
+        }
+    }
+
+    private NonTerminalNode root;
+
+    public NonTerminalNode getRoot() {
+        return this.root;
+    }
+
+    public ParseTree(int nonTerminal) {
+        this.root = new NonTerminalNode(nonTerminal, 0);
+    }
+
+    // recursive for now
+    private StringBuilder visit(
+            Node node,
+            StringBuilder accum,
+            Function<Integer, String> nonTerminalAlphabetInterpretation
+    ) {
+        accum.append(TAB).append(node.getNumber());
+        if (node instanceof TerminalNode) {
+            accum.append(SPACE + "[label=\"");
+            accum.append(((TerminalNode) node).getSymbol().getTag());
+            accum.append("\"]").append(NEWLINE);
+        } else {
+            NonTerminalNode ntNode = (NonTerminalNode) node;
+
+            accum.append(SPACE + "[label=\"");
+            int nonTerminal = ntNode.getSymbol();
+            accum.append(nonTerminalAlphabetInterpretation.apply(nonTerminal));
+            accum.append("\"]").append(NEWLINE);
+
+            for (Node child : ntNode.getChildren()) {
+                accum.append(TAB).append(node.getNumber())
+                        .append(SPACE + DOT_ARROW + SPACE)
+                        .append(child.getNumber())
+                        .append(NEWLINE);
+            }
+
+            for (Node child : ntNode.getChildren()) {
+                visit(child, accum, nonTerminalAlphabetInterpretation);
+            }
+        }
+        return accum;
+    }
+
+    public String toGraphvizDotString(Function<Integer, String> nonTerminalAlphabetInterpretation) {
+        String result = "digraph automaton {" +
+                NEWLINE +
+                TAB + "rankdir=TD;" + NEWLINE +
+                visit(this.root, new StringBuilder(), nonTerminalAlphabetInterpretation) +
+                "}" + NEWLINE;
+        return result;
+    }
+}
