41a54589ed76ba0dab1c752b3215bdd18dcac69b
sboyanovich <sergej.bojanovic3214@gmail.com>
2019-04-20 00:38:50 +0300

    Added exception for cases when input grammar is not LL(1).

---
+++ src/io/github/sboyanovich/scannergenerator/tests/l7/PredictionTableCreationException.java
@@ -0,0 +1,7 @@
+package io.github.sboyanovich.scannergenerator.tests.l7;
+
+public class PredictionTableCreationException extends Exception {
+    public PredictionTableCreationException(String message) {
+        super(message);
+    }
+}
