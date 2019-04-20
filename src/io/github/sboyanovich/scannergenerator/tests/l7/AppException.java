d7bfd7b0dbc1c8ad9c124591192d08c65dddd4fd
sboyanovich <sergej.bojanovic3214@gmail.com>
2019-04-20 15:32:10 +0300

    Added general exception class for application.

---
+++ src/io/github/sboyanovich/scannergenerator/tests/l7/AppException.java
@@ -0,0 +1,7 @@
+package io.github.sboyanovich.scannergenerator.tests.l7;
+
+public class AppException extends Exception {
+    public AppException(String message) {
+        super(message);
+    }
+}
