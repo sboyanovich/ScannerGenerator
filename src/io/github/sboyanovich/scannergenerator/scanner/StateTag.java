b28748b83c921257edb50ac75700908e45331b97
sboyanovich <sergej.bojanovic3214@gmail.com>
2020-03-03 01:30:38 +0300

    Finally, StateTag doesn't necessarily produce a Domain. For that, there is a suitable subtype DomainTag. Only such tags are to be allowed as final states of a first-gen (visitor pattern) scanner (this means no FINAL_DUMMY).

--- src/io/github/sboyanovich/scannergenerator/scanner/StateTag.java
+++ src/io/github/sboyanovich/scannergenerator/scanner/StateTag.java
@@ -1,18 +1,11 @@
 package io.github.sboyanovich.scannergenerator.scanner;
 
-import io.github.sboyanovich.scannergenerator.scanner.token.Domain;
-
 public interface StateTag {
     static boolean isFinal(StateTag tag) {
         return !tag.equals(StateTag.NOT_FINAL);
     }
 
     StateTag FINAL_DUMMY = new StateTag() {
-        @Override
-        public Domain getDomain() {
-            throw new Error("Dummy tag doesn't correspond to any domain!");
-        }
-
         @Override
         public String toString() {
             return "DUMMY";
@@ -21,19 +14,8 @@ public interface StateTag {
 
     StateTag NOT_FINAL = new StateTag() {
         @Override
-        public Domain getDomain() {
-            throw new Error("This is never supposed to be called!");
-        }
-
-        @Override
         public String toString() {
             return "NOT_FINAL";
         }
     };
-
-    Domain getDomain();
-
-    default boolean hasDomain() {
-        return true;
-    }
 }
