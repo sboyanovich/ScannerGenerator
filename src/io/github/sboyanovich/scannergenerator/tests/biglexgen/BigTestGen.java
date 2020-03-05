fd5b86fb0f7cc0f791bae091bc32b767809eb905
sboyanovich <sergej.bojanovic3214@gmail.com>
2020-03-06 00:28:18 +0300

    Added BigTest version which uses recognizers read from files.

---
+++ src/io/github/sboyanovich/scannergenerator/tests/biglexgen/BigTestGen.java
@@ -0,0 +1,55 @@
+package io.github.sboyanovich.scannergenerator.tests.biglexgen;
+
+import io.github.sboyanovich.scannergenerator.scanner.Message;
+import io.github.sboyanovich.scannergenerator.scanner.Position;
+import io.github.sboyanovich.scannergenerator.scanner.token.Domain;
+import io.github.sboyanovich.scannergenerator.scanner.token.Token;
+import io.github.sboyanovich.scannergenerator.utility.Utility;
+
+import java.util.*;
+
+public class BigTestGen {
+    public static void main(String[] args) {
+        String text = Utility.getText("testInputM1.txt");
+
+        MyScanner scanner = new MyScanner(text);
+        MockCompiler compiler = scanner.getCompiler();
+
+        Set<Domain> ignoredTokenTypes = Set.of(
+                Domain.END_OF_INPUT,
+                Domain.ERROR
+        );
+
+        List<Token> allTokens = new ArrayList<>();
+
+        int errCount = 0;
+
+        while (scanner.hasNext()) {
+            Token t = scanner.next();
+            allTokens.add(t);
+
+            if (!ignoredTokenTypes.contains(t.getTag())) {
+                System.out.println(t);
+            }
+            if (t.getTag() == Domain.ERROR) {
+                errCount++;
+                System.out.println(t.getCoords());
+            }
+        }
+
+        System.out.println();
+        System.out.println("Errors: " + errCount);
+        System.out.println("Compiler messages: ");
+        SortedMap<Position, Message> messages = compiler.getSortedMessages();
+        for (Map.Entry<Position, Message> entry : messages.entrySet()) {
+            System.out.println(entry.getValue() + " at " + entry.getKey());
+        }
+
+        if (errCount == 0) {
+            AST ast = Parser.parse(allTokens.iterator());
+            String dotAST = ast.toGraphVizDotString();
+            System.out.println();
+            System.out.println(dotAST);
+        }
+    }
+}
