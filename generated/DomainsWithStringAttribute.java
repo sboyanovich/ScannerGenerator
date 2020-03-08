3401f4e565485aa287c0d3df9c565432e04003ed
sboyanovich <sergej.bojanovic3214@gmail.com>
2020-03-08 17:06:01 +0300

    Generated up-to-date recognizers.

---
+++ generated/DomainsWithStringAttribute.java
@@ -0,0 +1,42 @@
+package io.github.sboyanovich.scannergenerator.generated;
+
+import io.github.sboyanovich.scannergenerator.scanner.Fragment;
+import io.github.sboyanovich.scannergenerator.scanner.Text;
+import io.github.sboyanovich.scannergenerator.scanner.token.DomainWithAttribute;
+import io.github.sboyanovich.scannergenerator.scanner.token.TokenWithAttribute;
+
+public enum DomainsWithStringAttribute implements DomainWithAttribute<String> {
+    NAMED_EXPR {
+        @Override
+        public String attribute(Text text, Fragment fragment) {
+
+        }
+
+        @Override
+        public TokenWithAttribute<String> createToken(Text text, Fragment fragment) {
+            return new TokenWithAttribute<>(fragment, NAMED_EXPR, attribute(text, fragment));
+        }
+    },
+    DOMAINS_GROUP_MARKER {
+        @Override
+        public String attribute(Text text, Fragment fragment) {
+
+        }
+
+        @Override
+        public TokenWithAttribute<String> createToken(Text text, Fragment fragment) {
+            return new TokenWithAttribute<>(fragment, DOMAINS_GROUP_MARKER, attribute(text, fragment));
+        }
+    },
+    IDENTIFIER {
+        @Override
+        public String attribute(Text text, Fragment fragment) {
+
+        }
+
+        @Override
+        public TokenWithAttribute<String> createToken(Text text, Fragment fragment) {
+            return new TokenWithAttribute<>(fragment, IDENTIFIER, attribute(text, fragment));
+        }
+    }
+}
