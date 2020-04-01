c8e29eb61f71fd22d66a69e0745b044d0df80c1e
sboyanovich <sergej.bojanovic3214@gmail.com>
2020-04-01 06:43:41 +0300

    Added warning and error counters to MessageList.

--- src/io/github/sboyanovich/scannergenerator/scanner/MessageList.java
+++ src/io/github/sboyanovich/scannergenerator/scanner/MessageList.java
@@ -6,17 +6,29 @@ import java.util.TreeMap;
 
 public class MessageList {
     private SortedMap<Position, Message> messages;
+    private int warningCount;
+    private int errorCount;
 
     public MessageList() {
         this.messages = new TreeMap<>();
     }
 
+    public int getWarningCount() {
+        return warningCount;
+    }
+
+    public int getErrorCount() {
+        return errorCount;
+    }
+
     public void addError(Position coord, String text) {
         this.messages.put(coord, new Message(true, text));
+        errorCount++;
     }
 
     public void addWarning(Position coord, String text) {
         this.messages.put(coord, new Message(false, text));
+        warningCount++;
     }
 
     public SortedMap<Position, Message> getSorted() {
